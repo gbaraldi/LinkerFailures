df220775dc20b6a6f171c1c052ee87c0794f08ad
Gabriel Baraldi <baraldigabriel@gmail.com>
2022-10-31 19:53:15 -0300

    Update buildarm.sh

--- reproducerarm/buildarm.sh
+++ reproducerarm/buildarm.sh
@@ -3,5 +3,5 @@ curl -L https://github.com/JuliaBinaryWrappers/armadillo_jll.jl/releases/downloa
 curl -L https://github.com/JuliaBinaryWrappers/OpenBLAS_jll.jl/releases/download/OpenBLAS-v0.3.21%2B3/OpenBLAS.v0.3.21.aarch64-apple-darwin-libgfortran5.tar.gz | tar - -xz
 install_name_tool -change /usr/lib/libc++.1.dylib @rpath/libc++.1.dylib lib/libarmadillo.11.2.1.dylib
 codesign --force --deep -s - lib/libarmadillo.11.2.1.dylib
-clang test2.c -o test2 -arch arm64
-./test2
+clang testarm.c -o testarm -arch arm64
+./testarm
