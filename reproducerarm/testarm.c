3fcc28bfe200447cae9ec8e4536ea55d4212f6b1
Gabriel Baraldi <baraldigabriel@gmail.com>
2022-10-31 20:00:00 -0300

    Dlopen libc++ before

--- reproducerarm/testarm.c
+++ reproducerarm/testarm.c
@@ -2,6 +2,7 @@
 #include <dlfcn.h>
 int main()
 {
+    dlopen("/usr/lib/libc++.1.dylib", 0x0001);
     dlopen("@executable_path/lib/libopenblas64_.0.3.21.dylib",  0x0001);
     printf("%s\n", dlerror());
     dlopen("@executable_path/lib/libarmadillo.11.2.1.dylib",0x0001);
